bdaae0fc7d9663b8f8b3e9050c5720fa3f51503f
Mark King <markpkng@gmail.com>
2020-02-28 12:38:34 -0500

    array ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -32,10 +32,13 @@ class RingBuffer:
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None] * capacity
 
     def append(self, item):
-        pass
+        self.storage[self.current] = item
+        self.current = self.current + 1 if self.current != self.capacity - 1 else 0
 
     def get(self):
-        pass
+        return [x for x in self.storage if x != None]
