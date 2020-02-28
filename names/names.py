c8663e54c97b3bc2514b67e90d87346ddf168718
Mark King <markpkng@gmail.com>
2020-02-28 13:37:20 -0500

    names stretch

--- names/names.py
+++ names/names.py
@@ -10,19 +10,29 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+starter_duplicates = []  # Return the list of duplicates in this data structure
 
-# start_time = time.time()
+start_time = time.time()
 
-# # starter code runtime: O(n ^ 2)
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
+# starter code runtime: O(n ^ 2)
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            starter_duplicates.append(name_1)
 
-# end_time = time.time()
-# print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-# print(f"runtime: {end_time - start_time} seconds")
+end_time = time.time()
+print(f"{len(starter_duplicates)} duplicates:\n\n{', '.join(starter_duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+
+# optimized list solution compared to original: O(n^2) but the outer loop continues if a name is found,
+# instead of always looping n times in inner loop as it did in the starter solution
+
+start_time = time.time()
+limited_duplicates = [name for name in names_1 if name in names_2]
+
+end_time = time.time()
+print(f"{len(limited_duplicates)} duplicates:\n\n{', '.join(limited_duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # runtime using binary search tree: O(n log(n))
 
@@ -41,3 +51,20 @@ print(f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# runtime using hash table: O(n)
+
+start_time = time.time()
+
+stretch_duplicates = []
+dictionary = {}
+
+
+for name in names_1:
+    dictionary[name] = name
+
+stretch_duplicates = [name for name in names_2 if name in dictionary]
+end_time = time.time()
+
+print(f"{len(stretch_duplicates)} duplicates:\n\n{', '.join(stretch_duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
